392d081657a67347550b1f2ceecfcc5f2d7db169
Aniket Kadam <anikadamg@gmail.com>
2024-02-15 11:24:53 +0530

    makie it live

--- src/app/page.js
+++ src/app/page.js
@@ -6,10 +6,21 @@ const MainPage = dynamic(() => import("./components"), {
   ssr: false,
   loading: () => <Loader />,
 });
+// export default function Home() {
+//   return (
+//     <div className="flex justify-center w-screen h-screen items-center bg-origin-content bg-repeat bg-sitebg">
+//       <h1 className="text-4xl tracking-wide text-white">Coming Soon!</h1>
+//     </div>
+//   );
+// }
 export default function Home() {
   return (
-    <div className="flex justify-center w-screen h-screen items-center bg-origin-content bg-repeat bg-sitebg">
-      <h1 className="text-4xl tracking-wide text-white">Coming Soon!</h1>
-    </div>
+    <main>
+      <section>
+        <MainPage>
+          <SiteContent></SiteContent>
+        </MainPage>
+      </section>
+    </main>
   );
 }
